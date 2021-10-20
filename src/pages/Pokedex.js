608fe49934ab69a4e0db6e01c08ab7e6d88f90fc
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-20 23:54:33 +0300

    data fetched from api

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,17 +1,41 @@
-import { Box } from '@mui/system'
-import React, { useEffect } from 'react'
+import { CircularProgress } from '@mui/material';
+import { Box } from '@mui/system';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { IMAGE_API_URL, POKEMON_API_URL } from '../config';
 
 function Pokedex() {
+    const [pokemonData, setPokemonData] = useState(null);
+    useEffect(() => {
+        axios.get(POKEMON_API_URL + '?limit=800').then((res) => {
+            if (res.status >= 200 && res.status < 300) {
+                const { results } = res.data;
+                let newPokemonData = [];
+                results.map((pokemon, index) => {
+                    index++;
+                    let pokemonObject = {
+                        id: index,
+                        url: IMAGE_API_URL + index + '.png',
+                        name: pokemon.name,
+                    };
+                    newPokemonData.push(pokemonObject);
+                });
+                setPokemonData(newPokemonData);
+            }
+        });
+    }, []);
 
-	useEffect(() => {
-		
-	}, [])
-
-	return (
-		<Box>
-			
-		</Box>
-	)
+    return (
+        <Box sx={{mt:10}}>
+            {pokemonData ? (
+                pokemonData.map((pokemon) => {
+                    return <h1>{pokemon.name}</h1>;
+                })
+            ) : (
+                <CircularProgress />
+            )}
+        </Box>
+    );
 }
 
-export default Pokedex
+export default Pokedex;
