6790a99107987e8f7827d232ca87d9f77382a9db
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-21 19:17:47 +0300

    card comp updated

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function PokemonCard({pokemon}) {
-	const { id, name, url } = pokemon
-	console.log(pokemon)
-	return (
-		<div>
-			hi
-		</div>
-	)
+const useStyles = makeStyles({
+    Card: {
+        cursor: 'pointer',
+    },
+    content: {
+        textAlign: 'center',
+    },
+    CardMedia: {
+        margin: 'auto',
+        width: 130,
+        height: 130,
+    },
+	link:{
+		textDecoration:"none"
+	}
+});
+
+function PokemonCard({ pokemon }) {
+    const { id, name, url } = pokemon;
+    const classes = useStyles();
+
+    return (
+        <Grid item xs={12} sm={2}>
+            <Link to={"/pokemon/" + id} className={classes.link}>
+                <Card className={classes.Card}>
+                    <CardMedia className={classes.CardMedia} image={url} />
+                    <CardContent className={classes.content}>
+                        <Typography variant="h6">{name}</Typography>
+                    </CardContent>
+                </Card>
+            </Link>
+        </Grid>
+    );
 }
 
-export default PokemonCard
+export default PokemonCard;
