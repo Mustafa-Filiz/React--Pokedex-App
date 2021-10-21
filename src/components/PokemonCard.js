2f29210784adad505e5a77a2163206429df150ba
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-21 17:30:21 +0300

    card comp created

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function PokemonCard({pokemon}) {
+	const { id, name, url } = pokemon
+	console.log(pokemon)
+	return (
+		<div>
+			hi
+		</div>
+	)
+}
+
+export default PokemonCard
