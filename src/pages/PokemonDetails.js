6667d81d87ff1dd036552ba291581fe0dbf95603
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-21 19:44:01 +0300

    fetched pokemon data

--- src/pages/PokemonDetails.js
+++ src/pages/PokemonDetails.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import { CircularProgress } from '@mui/material';
+import { Box } from '@mui/system';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { POKEMON_API_URL } from '../config';
 
-function PokemonDetails() {
-	return (
-		<div style={{marginTop: 50}}>
-			<h1>Hello</h1>
-		</div>
-	)
+function PokemonDetails(props) {
+    const [pokemon, setPokemon] = useState(null);
+
+    useEffect(() => {
+        const { id } = props.match?.params;
+        axios.get(POKEMON_API_URL + id).then((res) => setPokemon(res?.data));
+    }, []);
+
+    console.log(pokemon);
+
+    return (
+        <Box mt={10}>
+            {pokemon ? <h1>Hello</h1> : <CircularProgress />}
+        </Box>
+    );
 }
 
-export default PokemonDetails
+export default PokemonDetails;
