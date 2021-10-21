143f1682c866a5940e2713f834c86c10cc868c66
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-21 19:18:08 +0300

    details route added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Pokedex from './pages/Pokedex';
+import PokemonDetails from './pages/PokemonDetails';
 
 function App() {
     return (
         <Router>
             <NavBar />
             <Switch>
-                <Route path="/" component={Pokedex} />
+                <Route exact path="/" component={Pokedex} />
+                <Route exact path="/pokemon/:id" component={PokemonDetails} />
             </Switch>
         </Router>
     );
