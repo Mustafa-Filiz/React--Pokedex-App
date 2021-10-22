ef68b3c2e93a9139b6a2be0f2179e8a9b91e4582
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-22 23:03:17 +0300

    wrapped app comp with redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import NavBar from './components/NavBar';
 import Pokedex from './pages/Pokedex';
 import PokemonDetails from './pages/PokemonDetails';
+import { persistor, store } from './redux/Store';
 
 function App() {
     return (
-        <Router>
-            <NavBar />
-            <Switch>
-                <Route exact path="/" component={Pokedex} />
-                <Route exact path="/pokemon/:id" component={PokemonDetails} />
-            </Switch>
-        </Router>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <NavBar />
+                    <Switch>
+                        <Route exact path="/" component={Pokedex} />
+                        <Route
+                            exact
+                            path="/pokemon/:id"
+                            component={PokemonDetails}
+                        />
+                    </Switch>
+                </Router>
+            </PersistGate>
+        </Provider>
     );
 }
 
