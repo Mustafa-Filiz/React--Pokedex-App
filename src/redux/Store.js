ca79d5dfd176d6bd2ef9553d3b91707f2dd546ac
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-23 00:55:42 +0300

    reducer imported

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,14 +2,14 @@ import { createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-import rootReducer from './reducers'
+import pokemonReducer from './Reducer'
 
 const persistConfig = {
   key: 'root',
   storage,
 }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, pokemonReducer)
 export let store = createStore(persistedReducer)
 export let persistor = persistStore(store)
 
